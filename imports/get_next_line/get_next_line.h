5ed4d4e87670be58761b8b1758f3f0f99dc4653f
rferro-d <rapha4.lx@gmail.com>
2024-11-23 05:06:48 -0300

    added gnl but is need modified

---
+++ imports/get_next_line/get_next_line.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rferro-d <rferro-d@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/29 19:15:16 by rferro-d          #+#    #+#             */
+/*   Updated: 2024/10/29 19:15:51 by rferro-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+
+typedef struct s_line
+{
+	char			*content;
+	struct s_line	*next;
+}					t_line;
+
+char				*get_next_line(int fd);
+
+int					ft_strchr(const char *str, char c);
+t_line				*t_line_new_line(const char *str, int size);
+void				t_line_add_back(t_line **head, t_line *new);
+t_line				*t_line_get_last(t_line *head);
+void				t_line_clear(t_line **head);
+
+#endif
