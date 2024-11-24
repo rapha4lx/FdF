94bec4bafff9fecc758b42cc9a5925f6f5c96b53
rapha4x <rapha4.lx@gmail.com>
2024-11-24 00:16:34 -0300

    updated split

--- imports/libft/ft_split.c
+++ imports/libft/ft_split.c
@@ -6,95 +6,55 @@
 /*   By: rferro-d <rferro-d@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 18:22:05 by rferro-d          #+#    #+#             */
-/*   Updated: 2024/10/10 21:01:02 by rferro-d         ###   ########.fr       */
+/*   Updated: 2024/11/23 23:34:37 by rferro-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static	size_t	get_next_c(char const *s1, char c)
+static size_t	count_word(const char *s, char c)
 {
-	int	i;
+	size_t	i;
 
+	if (!*s)
+		return (0);
 	i = 0;
-	while (s1[i] && s1[i] != c)
-		i++;
+	while (*s)
+	{
+		while (*s == c)
+			s++;
+		if (*s)
+			i++;
+		while (*s != c && *s)
+			s++;	
+	}
 	return (i);
 }
 
-static size_t	start_buff(char ***buff, char const *s1, char c)
-{
-	size_t	i;
-	size_t	k;
-
-	i = 0;
-	k = 0;
-	while (s1[i])
-	{
-		while (s1[i] && s1[i] != c)
-			i++;
-		while (s1[i] && s1[i] == c)
-			i++;
-		k++;
-	}
-	*buff = (char **)malloc(sizeof(char *) * (k + 1));
-	return (k);
-}
-
-int	ccc(char **buff, size_t *i, size_t *j, char const *s1)
-{
-	size_t	it;
-
-	it = 0;
-	buff[*j] = (char *)malloc(sizeof(char) * (*i + 1));
-	if (buff[*j] == NULL)
-	{
-		while (it < *j)
-			free(buff[it++]);
-		free(buff);
-		return (0);
-	}
-	ft_strlcpy(buff[*j], s1, *i + 1);
-	*j = *j + 1;
-	return (1);
-}
-
-int	ddd(char **buff, char c, char const *s1, size_t buff_sz)
-{
-	size_t	i;
-	size_t	j;
-	size_t	it;
-
-	i = 0;
-	j = 0;
-	it = 0;
-	while (*s1 && buff_sz > j)
-	{
-		i = get_next_c(s1, c);
-		if (i != 0)
-		{
-			if (!ccc(buff, &i, &j, s1))
-				return (0);
-			s1 += i;
-		}
-		else
-			s1++;
-	}
-	buff[j] = NULL;
-	return (1);
-}
-
-char	**ft_split(char const *s1, char c)
+char	**ft_split(const char *s, char c)
 {
 	char	**buff;
-	size_t	buff_sz;
+	size_t	word_len;
+	int		i;
 
-	if (s1 == NULL)
-		return (NULL);
-	buff_sz = start_buff(&buff, s1, c);
-	if (buff == NULL)
-		return (NULL);
-	if (!ddd(buff, c, s1, buff_sz))
-		return (NULL);
+	buff = (char**)malloc(sizeof(char*) * (count_word(s, c) + 1));
+	if (!s || !buff)
+		return (0);
+	i = 0;
+	while (*s)
+	{
+		while (*s && *s == c)
+			s++;
+		if (*s)
+		{
+			if (!ft_strchr(s, c))
+				word_len = ft_strlen(s);
+			else
+				word_len = ft_strchr(s, c)  - s;
+			buff[i++] = ft_substr(s, 0, word_len);
+			s += word_len;
+		}
+	}
+	buff[i] = NULL;
 	return (buff);
 }
