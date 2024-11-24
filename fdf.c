c72bed112df1333bc428965b6e170eb726aaef91
rapha4x <rapha4.lx@gmail.com>
2024-11-24 00:17:15 -0300

    added t_map in maiin for before run program

--- fdf.c
+++ fdf.c
@@ -4,12 +4,14 @@
 
 int main(int argc, char **argv)
 {
+    t_map   *map;
+
     if (argc != 2)
     {
-        write(1, "map not inserted\n", 19);
+        write(1, "map not inserted\n", 18);
         return (0);
     }
     write (1, "asdlklakl\n", 11);
-    map_check(argv[1]);
+    map_check(argv[1], &map);
     return (0);
 }
