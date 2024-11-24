94c962bf02ac5c9cb204ed549904c8d631a78839
rapha4x <rapha4.lx@gmail.com>
2024-11-24 00:16:18 -0300

    started craete a parse functions free and alloc struct

--- srcs/parser.c
+++ srcs/parser.c
@@ -1,12 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rferro-d <rferro-d@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/23 20:05:17 by rferro-d          #+#    #+#             */
+/*   Updated: 2024/11/24 00:15:20 by rferro-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "parser.h"
 #include <fcntl.h>
 #include <stdio.h>
 #include "../includes/get_next_line.h"
+#include "../includes/libft.h"
 
-
-void    map_check(char *file)
+int		init_map(t_map **map)
 {
-    
-    get_next_line(file);
+	(*map) = (t_map*)malloc(sizeof(t_map) * 1);
+	if (*map)
+		return (0);
+	(*map)->status = 0;
+	(*map)->map_width = -1;
+	(*map)->map_heigh = -1;
+	(*map)->map_info = NULL;
+	return (1);
 }
+
+void	free_map(t_map *map)
+{
+	t_map_pointer	*tmp;
+
+	if (map->map_info)
+	{
+		while (map->map_info != NULL)
+		{
+			tmp = map->map_info;
+		}
+		
+	}
+	free(map);
+}
+
+int		map_line(t_map **map, char *line)
+{
+	char	*buff;
+	
+	buff = ft_split(line, ' ');
+
+
+}
+
+int		map_check(char *file, t_map **map)
+{
+    int		fd;
+    char	*line;
+	
+	fd = open(file, O_RDONLY);
+	line = get_next_line(fd);
+	init_map(map);
+    while(*map && line)
+	{
+		// if ((*map)->map_heigh == -1)
+		// 	(*map)->map_heigh = 
+		line = get_next_line(fd);
+	}
+	close(fd);
+	return (*map);
+}
+
+
--- srcs/parser.h
+++ srcs/parser.h
@@ -6,13 +6,28 @@
 /*   By: rferro-d <rferro-d@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/23 04:31:34 by rferro-d          #+#    #+#             */
-/*   Updated: 2024/11/23 04:55:00 by rferro-d         ###   ########.fr       */
+/*   Updated: 2024/11/24 00:13:24 by rferro-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSER_H
 # define PARSER_H
 
-int map_check(char *file);
+
+typedef	struct s_map_pointer
+{
+	int		value;
+	char	*hex;
+} t_map_pointer;
+
+typedef struct s_map
+{
+    int     status;
+	int		map_width;
+	int		map_heigh;
+	t_map_pointer **map_info;
+}       t_map;
+
+int map_check(char *file, t_map **map);
 
 #endif
