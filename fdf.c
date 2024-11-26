1d9bfd098c6b76c6eab40947f1a260b9575a44db
rferro-d <rapha4.lx@gmail.com>
2024-11-25 22:05:05 -0300

    new itens in main

--- fdf.c
+++ fdf.c
@@ -1,22 +1,30 @@
-
 #include <unistd.h>
+#include "imports/libft/libft.h"
+#include "srcs/parse/parser.h"
 
-#include "includes/libft.h"
-// #include "srcs/parser.h"
+void  map_error(char *map)
+{
+  ft_putstr("This map: ");
+  ft_putstr(map);
+  ft_putstr(" is brock\n");
+  exit(0);
+}
 
 int main(int argc, char **argv)
 {
-    (void)argc;
-    (void)argv;
-    // t_map   *map;
+    t_map   *map;
 
-    // if (argc != 2)
-    // {
-    //     write(1, "map not inserted\n", 18);
-    //     return (0);
-    // }
-    // write (1, "asdlklakl\n", 11);
-    // map_check(argv[1], &map);
-    ft_putchar_fd('d', 1);
+    if (argc != 2)
+    {
+        write(1, "map not inserted\n", 18);
+        return (0);
+    }
+    ft_putstr("[START] init_map\n");
+    if (!init_map(&map))
+		  return (0);
+    ft_putstr("[SUCCESS] init_map\n");
+    ft_putstr("[START] map_check\n");
+    if (!map_check(argv[1], &map))
+      map_error(argv[1]);
     return (0);
 }
