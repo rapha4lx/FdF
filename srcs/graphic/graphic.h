66bbe9b49f78c1f1b686d855868bec2026847443
rapha4x <rapha4.lx@gmail.com>
2024-12-01 03:27:09 -0300

    init_hook now return status. init_window receive map with argument. free_window return status. t_window now storage screen size

--- srcs/graphic/graphic.h
+++ srcs/graphic/graphic.h
@@ -6,26 +6,38 @@
 /*   By: rferro-d <rferro-d@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/30 18:44:00 by rferro-d          #+#    #+#             */
-/*   Updated: 2024/11/30 23:43:21 by rferro-d         ###   ########.fr       */
+/*   Updated: 2024/12/01 03:07:53 by rferro-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GRAPHIC_H
 # define GRAPHIC_H
 
+#include "../parse/parser.h"
+
+#define CREATE_DISPLAY_FAIL "Fail set up connection to the graphical system\n"
+#define FAIL_GET_SCREEN_SIZE "Fail get screen size"
+#define CREATE_WINDOW_FAIL "No Success to create window"
+#define INIT_HOOK_FAIL "Init Hook Fail"
+
 typedef struct s_window
 {
 	void	*mlx;
 	void	*win;
+
+	int		sizex;
+	int		sizey;
+	
+	t_map * map;
 }	t_window;
 
 
 void	free_pointer(void **pointer);
 
-void	init_window(void);
-void	free_window(t_window *window);
+void	init_window(t_map *map);
+int		free_window(t_window *window);
 
 
 void	free_hook(t_window **window);
-void	init_hook(t_window *window);
+int		init_hook(t_window *window);
 #endif
