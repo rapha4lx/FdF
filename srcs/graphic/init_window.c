93a0b05c5af32aee638da149d9a13ad22fc4a551
rapha4x <rapha4.lx@gmail.com>
2024-12-01 03:35:18 -0300

    free_pointer removed. init_window finished function. free_window if added for check if window is loaded and not free

--- srcs/graphic/init_window.c
+++ srcs/graphic/init_window.c
@@ -6,38 +6,52 @@
 /*   By: rferro-d <rferro-d@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/30 18:43:38 by rferro-d          #+#    #+#             */
-/*   Updated: 2024/11/30 23:09:07 by rferro-d         ###   ########.fr       */
+/*   Updated: 2024/12/01 03:15:30 by rferro-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "graphic.h"
+#include "../../imports/libft/libft.h"
 #include "../../imports/minilibx-linux/mlx.h"
+#include "graphic.h"
 #include <stdlib.h>
 
-void	free_pointer(void **pointer)
+int		free_window(t_window *window)
 {
-	if (!(*pointer))
-		return ;
-	free((*pointer));
-	(*pointer) = NULL;
+	if (window->mlx && window->win)
+	{
+		mlx_clear_window(window->mlx, window->win);
+		mlx_destroy_window(window->mlx, window->win);
+		mlx_loop_end(window->mlx);
+		window->win = NULL;
+	}
+	return (1);
 }
 
-void	free_window(t_window *window)
+void	init_window(t_map *map)
 {
-	mlx_clear_window(window->mlx, window->win);
-	mlx_destroy_window(window->mlx, window->win);
-	free_pointer(&window->win);
-	mlx_destroy_display(window->mlx);
-	free_pointer(&window->mlx);
-}
+	t_window	window;
 
-void	init_window(void)
-{
-	t_window	*window;
-
-	window = (t_window*)malloc(sizeof(t_window));
-	window->mlx = mlx_init();
-	window->win = mlx_new_window(window->mlx, 300, 300, "FdF");
-	init_hook(&window);
-	mlx_loop(window->mlx);
+	window.map = map;
+	if (!(window.mlx = mlx_init()))
+		return (ft_putstr (CREATE_DISPLAY_FAIL)); 
+	if (!mlx_get_screen_size(window.mlx, &window.sizex,
+		&window.sizey))
+	{
+		mlx_destroy_display(window.mlx);
+		return (ft_putstr(FAIL_GET_SCREEN_SIZE));
+	}
+	window.win = mlx_new_window(window.mlx, window.sizex
+	,window.sizey, "FdF");
+	if (!window.win)
+	{
+		mlx_destroy_display(window.mlx);
+		return (ft_putstr(CREATE_WINDOW_FAIL));
+	}
+	if (init_hook(&window))
+		mlx_loop(window.mlx);
+	else
+		ft_putstr(INIT_HOOK_FAIL);
+	free_window(&window);
+	mlx_destroy_display(window.mlx);
+	free(window.mlx);
 }
