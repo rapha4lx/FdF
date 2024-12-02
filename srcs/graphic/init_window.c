66ebe7456461bf517be55d395f1c3e6036d4913a
rapha4x <rapha4.lx@gmail.com>
2024-12-01 22:34:22 -0300

    added free_images in free_window

--- srcs/graphic/init_window.c
+++ srcs/graphic/init_window.c
@@ -6,7 +6,7 @@
 /*   By: rferro-d <rferro-d@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/30 18:43:38 by rferro-d          #+#    #+#             */
-/*   Updated: 2024/12/01 17:41:03 by rferro-d         ###   ########.fr       */
+/*   Updated: 2024/12/01 22:09:34 by rferro-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,10 @@
 
 int	free_window(t_window *window)
 {
+	free_images(window);
 	if (window->mlx && window->win)
 	{
-		mlx_clear_window(window->mlx, window->win);
 		mlx_destroy_window(window->mlx, window->win);
-		mlx_loop_end(window->mlx);
 		window->win = NULL;
 	}
 	return (1);
@@ -63,7 +62,7 @@ void	init_window(t_map *map)
 		mlx_destroy_display(window.mlx);
 		return (ft_putstr(CREATE_WINDOW_FAIL));
 	}
-	if (init_hook(&window))
+	if (init_hook(&window) && init_images(&window))
 		mlx_loop(window.mlx);
 	else
 		ft_putstr(INIT_HOOK_FAIL);
