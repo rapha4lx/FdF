cb7ba57c8f0dbeec0a48c133caebf32d86ed6063
rapha4x <rapha4.lx@gmail.com>
2024-12-01 22:33:51 -0300

    removed ft_pustr for debug and fixed keyboard using free_window

--- srcs/graphic/init_hook.c
+++ srcs/graphic/init_hook.c
@@ -6,7 +6,7 @@
 /*   By: rferro-d <rferro-d@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/30 18:58:32 by rferro-d          #+#    #+#             */
-/*   Updated: 2024/12/01 17:45:02 by rferro-d         ###   ########.fr       */
+/*   Updated: 2024/12/01 22:28:32 by rferro-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@
 
 static int	close_from_button(t_window *window)
 {
+	// free_window(window);
 	mlx_loop_end(window->mlx);
 	return (0);
 }
@@ -26,7 +27,8 @@ static int	close_from_button(t_window *window)
 static int	keyboard_hook(int keycode, t_window *window)
 {
 	if (keycode == 53 || keycode == 65307)
-		return (free_window(window));
+		return (mlx_loop_end(window->mlx));
+		// return (free_window(window));
 	return (0);
 }
 
@@ -34,18 +36,15 @@ int	init_hook(t_window *window)
 {
 	if (!mlx_hook(window->win, ON_KEYDOWN, KeyPressMask, keyboard_hook, window))
 		return (0);
-	ft_putstr("SUCCESS keyboard_hook\n");
 	if (!mlx_hook(window->win, ON_DESTROY, NoEventMask,
 			close_from_button, window))
 		return (0);
-	ft_putstr("SUCCESS close_from_button\n");
-	if (!mlx_hook(window->win, ON_MOUSEDOWN, Button1MotionMask,
-			set_mouse_down, &window->mouse))
-		return (0);
-	ft_putstr("SUCCESS mouse_down\n");
-	if (!mlx_hook(window->win, ON_MOUSEUP, Button1MotionMask,
-			set_mouse_up, &window->mouse))
-		return (0);
-	ft_putstr("SUCCESS mouse_up\n");
+	// if (!mlx_hook(window->win, ON_MOUSEDOWN, ButtonPressMask,
+	// 		set_mouse_down, &window->mouse))
+	// 	return (0);
+	// if (!mlx_hook(window->win, ON_MOUSEUP, ButtonPressMask,
+	// 		set_mouse_up, &window->mouse))
+	// 	return (0);
+	// render(window);
 	return (1);
 }
